1505d4cb7246faaa66611e90d5f0d301e6c013e3
fatihbucak56-beep <fatihbucak56@gmail.com>
2026-08-13 20:05:16 +0200

    build: Web-App neu gebaut mit allen Fixes
    - Freund-Chat funktioniert jetzt (friend-chat Endpunkte)
    - Bidirektionale Verbindung über Backend
    - Moderner Chat-Titel (nur Name, kein 'Chat mit')
    - Referral-Code-Persistenz (SharedPrefs auch ohne Login)
    - Willkommen-SnackBar nur einmal anzeigen

--- webapp/flutter_bootstrap.js
+++ webapp/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2608075341" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1630517113" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
